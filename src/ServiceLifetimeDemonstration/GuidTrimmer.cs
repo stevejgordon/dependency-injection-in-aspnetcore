334fb8c14070e1dc9cfa9f694e2d296bc7f0bca0
Steve Gordon <sgordon@hotmail.co.uk>
2021-11-12 14:27:26 +0000

    Add service lifetime demo

---
+++ src/ServiceLifetimeDemonstration/GuidTrimmer.cs
@@ -0,0 +1,18 @@
+﻿namespace ServiceLifetimeDemonstration;
+
+public class GuidTrimmer : IGuidTrimmer
+{
+	private readonly GuidService _guidService;
+
+	public GuidTrimmer(GuidService guidService)
+	{
+		_guidService = guidService;
+	}
+
+	public string TrimmedGuid()
+	{
+		var guid = _guidService.GetGuid();
+		var trimmed = guid.Replace("-", string.Empty);
+		return trimmed;
+	}
+}
---
+++ src/ServiceLifetimeDemonstration/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceLifetimeDemonstration.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+		private readonly GuidService _guidService;
+
+		public IndexModel(ILogger<IndexModel> logger, GuidService guidService)
+        {
+            _logger = logger;
+			_guidService = guidService;
+		}
+
+		public string Guid { get; private set; } = "Missing";
+
+		public string GuidFromMiddleware { get; private set; } = "Missing";
+
+        public void OnGet()
+        {
+			var guid = _guidService.GetGuid();
+
+			Guid = guid;
+
+			if (HttpContext.Items.TryGetValue(CustomMiddleware.MiddlewareGuid, out var mwGuid) && mwGuid is string middlewareGuid)
+			{
+				GuidFromMiddleware = middlewareGuid;
+			}
+
+			_logger.LogInformation($"Controller: The GUID from the GuidService is {guid}");
+		}
+    }
+}
