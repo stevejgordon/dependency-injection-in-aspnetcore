a64441475cc5fa8d2d6781fe3d92343837fab573
Steve Gordon <sgordon@hotmail.co.uk>
2021-11-16 07:11:18 +0000

    Add booking rules

---
+++ src/TennisBookings/Services/Bookings/Rules/ClubIsOpenRule.cs
@@ -0,0 +1,22 @@
+﻿namespace TennisBookings.Services.Bookings.Rules
+{
+	public class ClubIsOpenRule : ISingletonCourtBookingRule
+{
+		private readonly IClubConfiguration _clubConfiguration;
+
+		public ClubIsOpenRule(IClubConfiguration clubConfiguration)
+		{
+			_clubConfiguration = clubConfiguration;
+		}
+
+		public Task<bool> CompliesWithRuleAsync(CourtBooking booking)
+		{
+			var startHourPasses = booking.StartDateTime.Hour >= _clubConfiguration.OpenHour;
+			var endHourPasses = booking.EndDateTime.Hour <= _clubConfiguration.CloseHour;
+
+			return Task.FromResult(startHourPasses && endHourPasses);
+		}
+
+		public string ErrorMessage => "Can't make a booking when the club is closed";
+	}
+}
---
+++ src/TennisBookings/Services/Bookings/Rules/ICourtBookingRule.cs
@@ -0,0 +1,19 @@
+using TennisBookings.Configuration;
+
+namespace TennisBookings.Services.Bookings.Rules
+{
+	public interface ICourtBookingRule
+	{
+		Task<bool> CompliesWithRuleAsync(CourtBooking booking);
+
+		string ErrorMessage { get; }
+	}
+
+	public interface IScopedCourtBookingRule : ICourtBookingRule
+	{
+	}
+
+	public interface ISingletonCourtBookingRule : ICourtBookingRule
+	{
+	}
+}
